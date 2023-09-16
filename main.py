e0c502074c1560ee282b2f2b35fc3cbf3ae65a51
raquested <rae.ingobo@gmail.com>
2023-09-16 15:32:08 -0400

    so this slider is definitely doing a thing :

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ value = 10
 page = """
 #HopHacks23: [Insert Title Here]
 
-On a scale of 1 to 100, how ill are you feeling today? <|{value}|> <br/>
+ahh literally what is this <|{value}|> <br/>
 <|{value}|slider|>
 
 """
