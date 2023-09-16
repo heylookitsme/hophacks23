728f87c59f6d74fae7cc6ee7aee4c11e5a83176d
Rae <rachelw@terpmail.umd.edu>
2023-09-16 17:25:12 -0400

    tests with different taipy components and got some diff pages as a substitute for screens. TODO: how to inject custom javascript

--- main.py
+++ main.py
@@ -1,13 +1,68 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, Markdown, Html
 
 value = 10
 
-page = """
-#HopHacks23: [Insert Title Here]
+page = section2 = """
+help
+#Our Very First Taipy Application
 
-ahh literally what is this <|{value}|> <br/>
+<|layout|columns=1 1 1|
+
+<|first column
+<|container container-styling|
+###Slider 1 <br/>
+Slider value: <|{value}|> <br/>
 <|{value}|slider|>
+|>
+|>
+
+<|second column
+<|container container-styling|
+###Slider 2 <br/>
+Slider value: <|{value}|> <br/>
+<|{value}|slider|>
+|>
+|>
+
+<|third column
+<|container container-styling|
+###Slider 3 <br/>
+Slider value: <|{value}|> <br/>
+<|{value}|slider|>
+|>
+|>
+|>
+
+<div>hiiiii</div>
+
+<a href="/page1"> next page</a>
 
 """
 
-Gui(page).run(use_reloader=True, port=5001)
+md_page = Markdown("""
+# Page title
+
+Any [_Markdown_](https://en.wikipedia.org/wiki/Markdown) content can be used here.
+                   
+you can't escape because you cant add links in markdown LUL 
+""")
+
+html_page = Html("""
+<h1>Page title</h1>
+
+Any <a href="https://en.wikipedia.org/wiki/HTML"><i>HTML</i></a>
+content can be used here.
+                 
+<a href="/page1"> next page</a>
+""")
+
+pages = {
+    'page1': md_page,
+    'page2': html_page
+}
+
+
+content = "average_age.csv"
+
+# Gui(page).run(use_reloader=True, port=5001)
+Gui(pages=pages).run(use_reloader=True, port=5001)
